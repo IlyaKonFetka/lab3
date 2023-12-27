0877b2083b7ff9ed676b00642e3b1ce6a843a374
Ilya K <simalin2020@gmail.com>
2023-12-27 20:38:43 +0500

    upd 27.12

---
+++ src/main/java/itmo/interfaces/Flyable.java
@@ -0,0 +1,2 @@
+package itmo.interfaces;public interface Flyable {
+}
--- src/main/java/itmo/interfaces/SuperProperty.java
+++
@@ -1,6 +0,0 @@
-package itmo.interfaces;
-
-public interface SuperProperty {
-    public final String letter = "Property";
-    //Main class for polymorphism realization with inProperty and outProperty
-}
